b0e1728156ed844a7551519f8117fa1ea79c0396
Jean Detoeuf <thebignet@gmail.com>
2014-02-18 19:18:49 +0100

    Menu avec bouton actif

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,15 +1,28 @@
+/*global _:false */
 'use strict';
 
 angular.module('testYeomanApp')
-.controller('NavCtrl', function ($scope,$location) {
+.controller('NavCtrl', function ($scope,$location,$rootScope) {
 	$scope.menus = [
-		{'link':'', 'titre':'Accueil'},
-		{'link':'auteur', 'titre':'Auteur'},
-		{'link':'portfolio', 'titre':'Portfolio'},
-		{'link':'tarifs', 'titre':'Tarifs'},
-		{'link':'contact', 'titre':'Contact'}
+		{'link':'/', 'titre':'Accueil'},
+		{'link':'/auteur', 'titre':'Auteur'},
+		{'link':'/portfolio', 'titre':'Portfolio'},
+		{'link':'/tarifs', 'titre':'Tarifs'},
+		{'link':'/contact', 'titre':'Contact'}
 	];
 	$scope.goTo = function(link){
-		$location.path('/'+link);
+		$location.path(link);
 	};
+	$rootScope.$on('$locationChangeStart', function (event, next, current) {
+		setActive($location.path());
+		console.log('change route '+$location.path());
+		console.log(event);
+		console.log(next);
+		console.log(current);
+	});
+	function setActive(route){
+		_.each($scope.menus,function(item){
+			item.active=(item.link===route);
+		});
+	}
 });
