113dd5256abfa61b01f83e1e70b50366810c7570
Jean Detoeuf <thebignet@gmail.com>
2014-02-18 17:32:12 +0100

    Script MEP Raspberry

---
+++ mepRaspberry.sh
@@ -0,0 +1,5 @@
+tar cvf dist.tar dist/;
+gzip -f dist.tar;
+scp dist.tar.gz pi@192.168.1.47:/tmp/
+ssh pi@192.168.1.47 "cd /var/www; sudo rm -Rf avotreimage/; sudo tar xvf /tmp/dist.tar.gz; sudo mv dist avotreimage/"
+rm -f dist.tar.gz
