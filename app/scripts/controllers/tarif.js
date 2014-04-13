dfceb85b8336255d1dc20eb49b3251d0cd636dae
Jean Detoeuf <thebignet@gmail.com>
2014-04-13 20:04:15 +0200

    Changement tarifs

--- app/scripts/controllers/tarif.js
+++ app/scripts/controllers/tarif.js
@@ -3,8 +3,8 @@
 angular.module('aVotreImage')
 .controller('TarifCtrl', function ($scope) {
 	$scope.tarifs = [
-		{nom:'Coup de crayon',nb:1,prix:200},
-		{nom:'Coup de pinceau',nb:2,prix:300},
-		{nom:'Coup de foudre',nb:3,prix:400}
+		{nom:'Coup de crayon',nb:1,prix:199},
+		{nom:'Coup de pinceau',nb:2,prix:299},
+		{nom:'Coup de foudre',nb:3,prix:399}
 	];
 });
